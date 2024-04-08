b9c135b5554f021681e9550e44da626ece81eb68
one1erp <avigaile@one1.co.il>
2024-04-08 13:00:11 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Forms;
+namespace Protection_RHP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold);
+            this.lblHeader.Location = new System.Drawing.Point(23, 18);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(988, 35);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = ".נכנסת למערכת מידע המכילה מידע רגיש. יש להקפיד על שמירת המידע כנדרש בחוק הגנת הפר" +
+    "טיות";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblHeader);
+            this.Name = "Form1";
+            this.Size = new System.Drawing.Size(1059, 64);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lblHeader;
+
+
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+using LSExtensionExplorer;
+
+
+namespace Protection_RHP
+{
+    public partial class Form1 : UserControl, ILSXplVisualControl, ILSXplSystemControl
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void PreDisplay()
+        {
+   
+        }
+
+
+
+        public void BeforeFocusedNodeChange(string keyData)
+        {
+      //      throw new System.NotImplementedException();
+        }
+
+        public void ChangeDataExplorerView(DataExplorerViewStyles style)
+        {
+     //       throw new System.NotImplementedException();
+        }//
+
+        public void DataExplorerToolbarButtonClicked(ToolbarItem item)
+        {
+         //   throw new System.NotImplementedException();
+        }
+
+        public event ExceptionThrownEventHandler ExceptionThrown;
+
+        public void FocusedNodeChanged(string keyData)
+        {
+       //     throw new System.NotImplementedException();
+        }
+
+        public string GetObjectsStaticItemText()
+        {
+            return "";// throw new System.NotImplementedException();
+        }
+
+        public void InitializeToolbarItemsStatus(ref Hashtable toolbarItems)
+        {
+         //   throw new System.NotImplementedException();
+        }
+
+        public void NeedRefresh(string keyData, params string[] parameters)
+        {
+       //     throw new System.NotImplementedException();
+        }
+
+        public void ProcessToolbarItemClick(ToolbarItem item)
+        {
+      //      throw new System.NotImplementedException();
+        }
+
+        public void SetServiceProvider(object serviceProvider)
+        {
+          //  throw new System.NotImplementedException();
+        }
+
+        
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            lblHeader.Location = new Point(Width / 2 - lblHeader.Width / 2, lblHeader.Location.Y);
+         //   pictureBox1.Location = new Point(Width / 2 - pictureBox1.Width / 2, pictureBox1.Location.Y);
+            
+        }
+    }
+}
